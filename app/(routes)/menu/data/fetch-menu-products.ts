2a0a89e46844a91d2defb7fa437097296fbad758
Parham Roohi <144388104+ParhamRoohi@users.noreply.github.com>
2025-09-25 18:17:12 +0330

    fix: changed the fetching mechanism

--- app/(routes)/menu/data/fetch-menu-products.ts
+++ app/(routes)/menu/data/fetch-menu-products.ts
@@ -5,12 +5,12 @@ const client = createClient({
   projectId: "gw9pds6y",
   dataset: "production",
   apiVersion: "2023-01-01",
-  useCdn: true,
+  useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 });
 
 const PRODUCT_QUERY = `*[_type == "product"]{productId, title, productImage, description, category, price}`;
 
 export async function fetchMenuProducts(): Promise<Product[]> {
-  return client.fetch(PRODUCT_QUERY);
+  return client.fetch(PRODUCT_QUERY, {}, { cache: "no-store" });
 }
